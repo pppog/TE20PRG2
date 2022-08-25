af5b9dafe79a90a7d3294b96adef38f9f9ca1a75
pppog <77719393+pppog@users.noreply.github.com>
2022-08-25 13:50:42 +0200

    gjorde 2 uppgifter, började på tredje

--- Greetings.java
+++ Greetings.java
@@ -2,14 +2,15 @@ import javax.swing.*;
 
 public class Greetings {
     public static void main(String[] args) {
-        String NText = "h";
+        String NText = "";
         String Text = JOptionPane.showInputDialog("Write something");
-        for(int i=1;i<Text.length(); i++) {
-            if(Text.charAt(i) == 'e') {
+        for(int i=0;i<Text.length(); i++) {
+            if (Text.charAt(i) == 'e') {
                 NText += "ee";
+            } else {
+                NText += Text.charAt(i);
             }
         }
-        NText += "y";
         System.out.println(NText);
     }
 }
