709933583da9096c17f8525747468b096e3aa5a2
pppog <77719393+pppog@users.noreply.github.com>
2022-08-26 12:33:10 +0200

    gjorde 2 uppgifter, började på tredje

--- humancannonball.java
+++ humancannonball.java
@@ -7,7 +7,31 @@ public class humancannonball {
         int x = 0;
         int l = 0;
         int u = 0;
+        int space = 0;
         String input = JOptionPane.showInputDialog("Parameters?");
-
+        for (int i=0; i<input.length(); i++) {
+            if (input.charAt(i)==' ') {
+                space += 1;
+            }
+            else {
+                if(space==0) {
+                    v += input.charAt(i);
+                }
+                if(space==1) {
+                    a += input.charAt(i);
+                }
+                if(space==2) {
+                    x += input.charAt(i);
+                }
+                if(space==3) {
+                    l += input.charAt(i);
+                }
+                if(space==4) {
+                    u += input.charAt(i);
+                    space = 0;
+                }
+            }
+        }
+        System.out.println("v=" + v + " a=" + a + " x=" + x + " l=" + l + " u=" + u);
     }
 }
